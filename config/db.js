4f2d43599fd42ca584fa9d371543d62fc7a504ee
Prem Raj <premraj299341@gmail.com>
2026-07-27 00:38:01 +0530

    feat: implement MongoDB connection utility with global caching for serverless environments

--- config/db.js
+++ config/db.js
@@ -20,6 +20,9 @@ export async function connectDB() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
+      serverSelectionTimeoutMS: 10000,
+      connectTimeoutMS: 10000,
+      maxPoolSize: 10,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongooseInstance) => {
