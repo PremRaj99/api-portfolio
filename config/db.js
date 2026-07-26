c0cee66dd23de57f2096abea200fb214c6c17c15
Prem Raj <premraj299341@gmail.com>
2026-07-27 00:45:11 +0530

    feat: implement MongoDB connection utility with global caching support

--- config/db.js
+++ config/db.js
@@ -20,9 +20,6 @@ export async function connectDB() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
-      serverSelectionTimeoutMS: 10000,
-      connectTimeoutMS: 10000,
-      maxPoolSize: 10,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongooseInstance) => {
